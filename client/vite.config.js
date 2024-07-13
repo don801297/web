48af0747e4587fc57795d4c6134ac2efca4a7c31
don801297 <don801297@gmail.com>
2024-07-13 19:04:05 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server:{
+    host: true,
+    
+  },
+  
+  plugins: [react()],
+  define:{
+    'process.env.VITE' :JSON.stringify(process.env.VITE_KEY)
+  },
+  
+});
