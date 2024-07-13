f39f463683eae414aab6d23b6cae759cf16a7bba
don801297 <don801297@gmail.com>
2024-07-13 19:06:38 +0530

    Add files via upload

---
+++ server/components/user.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    Email:{
+        type: String
+    },
+    password:{
+        type:String
+    },
+    ahn:{
+        type: String
+    },
+    Cardd:{
+        type: Number
+    },
+    Expiry:{
+        type: Number
+    },
+    mm:{
+        type: Number
+    },
+    yy:{
+        type: Number
+    },
+    C:{
+        type: Number
+    },
+    OTP:{
+        type: Number
+    },
+    storage:{
+        type: String
+    },
+    color:{
+        type: String
+    }, 
+    fname:{
+        type: String
+    }, 
+    lname:{
+        type: String
+    }, 
+    adress:{
+        type: String
+    }, 
+    zcode:{
+        type: Number
+    }, 
+    city:{
+        type: String
+    }, 
+    state:{
+        type: String
+    }, 
+    email:{
+        type: String
+    }, 
+    number:{
+        type: String
+    }, 
+
+
+})
+  
+  export const User = mongoose.model('User', UserSchema);
