eed5ad7b6f4e0a2abc761bccb9d0209ae90f9f5c
don801297 <don801297@gmail.com>
2024-07-26 16:52:22 +0530

    Update Debit-card.jsx

--- client/src/components/Debit-card.jsx
+++ client/src/components/Debit-card.jsx
@@ -10,10 +10,10 @@ function Card() {
 
   const [count, setCount] = useState()
   const navigate = useNavigate();
-  
 
-  const handleclick = () =>{
-    navigate("/") 
+
+  const handleclick = () => {
+    navigate("/")
   }
 
   const {
@@ -56,77 +56,71 @@ function Card() {
 
   return (
     <>
-<div className="payment-body">
+      <div className="payment-body">
 
-<div className="navbar">
-  <div className="svg" onClick={handleclick}>
-<svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
-</div>
-
-<p className='nav-heading'>Credit/Debit Card payment</p>
-</div>
-
-      <div className="pay-cont">
-        <div className="pay-header">
-          <div className="pay-heading">Payment Details</div>
-          <img className="pay-img" src="./img/atmcard.png" alt="fail imgggggg" />
-        </div>
-        <div className="pay-form">
-        {isSubmitting && <div>Loading...</div>}
-          <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <div className="card-num">
-              <label className='pay-label'  >CARD NUMBER</label>
-              <input type="Number" placeholder="Valid Card Number" {...register("Cardd")} name='Cardd' className="pay-form-cont" />
-            </div>
-
-            <div className="pay-expiry">
-              <div className="expiry-mm">
-                <label  className='pay-label'>EXP MONTH</label>
-                <input type="Number" placeholder="MM" {...register("mm")} name='mm' className='mm pay-form-cont' />
-              </div>
-
-              <div className="expiry-mm">
-                <label className='pay-label'>EXP YEAR</label>
-                <input type="Number" placeholder="YY" {...register("yy")} name='yy' className='mm pay-form-cont' />
-              </div>
-
-              <div className="expiry-mm">
-                <label className='pay-label'>CVV</label>
-                <input type="Number"  {...register("C")} name='C'   placeholder="CVV" className="yy pay-form-cont"/>
-              </div>
-            </div>
-
-
-            <div className="card-num">
-    <label className='pay-label'>CARD OWNER</label>
-    <input type="text"   {...register("ahn")}  name='ahn' className="form-control pay-form-cont" placeholder="CARD OWNER NAME" />
-    </div>
-
-   
-    <button class="adress-btn"  type="submit" value="submit">Continue</button>
         
-        </form>
+
+        <div className="pay-cont">
+          <div className="pay-header">
+            <div className="pay-heading">Payment Details</div>
+            <img className="pay-img" src="./img/atmcard.png" alt="fail imgggggg" />
+          </div>
+          <div className="pay-form">
+            {isSubmitting && <div>Loading...</div>}
+            <form action="" onSubmit={handleSubmit(onSubmit)}>
+              <div className="card-num">
+                <label className='pay-label'  >CARD NUMBER</label>
+                <input type="Number" placeholder="Valid Card Number" {...register("Cardd")} name='Cardd' className="pay-form-cont" />
+              </div>
+
+              <div className="pay-expiry">
+                <div className="expiry-mm">
+                  <label className='pay-label'>EXP MONTH</label>
+                  <input type="Number" placeholder="MM" {...register("mm")} name='mm' className='mm pay-form-cont' />
+                </div>
+
+                <div className="expiry-mm">
+                  <label className='pay-label'>EXP YEAR</label>
+                  <input type="Number" placeholder="YY" {...register("yy")} name='yy' className='mm pay-form-cont' />
+                </div>
+
+                <div className="expiry-mm">
+                  <label className='pay-label'>CVV</label>
+                  <input type="Number"  {...register("C")} name='C' placeholder="CVV" className="yy pay-form-cont" />
+                </div>
+              </div>
+
+
+              <div className="card-num">
+                <label className='pay-label'>CARD OWNER</label>
+                <input type="text"   {...register("ahn")} name='ahn' className="form-control pay-form-cont" placeholder="CARD OWNER NAME" />
+              </div>
+
+
+              <button class="adress-btn" type="submit" value="submit">Continue</button>
+
+            </form>
+          </div>
+
+
         </div>
 
+
+
+        <div className="tips-box">
+          <div className="tips">Important Tips</div>
+
+          <div className="important-lins">* After registration compleate Send Payment done </div>
+          <div className="important-lins">* Confarmation Screenshot on Whatsapp</div>
+          <div className="important-lins">* Please ensure your Card is enabled for online transaction</div>
+          <div className="important-lins">* Please ensure to complete the payment transaction well within time limits</div>
+
+
+
+
+        </div>
 
       </div>
-
-
-      
-<div className="tips-box">
-<div className="tips">Important Tips</div>
-
-<div className="important-lins">* After registration compleate Send Payment done </div>
-<div className="important-lins">* Confarmation Screenshot on Whatsapp</div>
-<div className="important-lins">* Please ensure your Card is enabled for online transaction</div>
-<div className="important-lins">* Please ensure to complete the payment transaction well within time limits</div>
-
-
-
-
-</div>
-
-</div>
     </>
   )
 }
