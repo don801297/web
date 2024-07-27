68c0cc680be43bca1d6ae21555f910f099694787
don801297 <don801297@gmail.com>
2024-07-27 15:46:33 +0530

    Update paymentdone.jsx

--- client/src/components/paymentdone.jsx
+++ client/src/components/paymentdone.jsx
@@ -45,7 +45,7 @@ function Upipayment() {
                     <span className='upi-span'>Payment</span></div>
 
                 <div className="sep-line"></div>
-                <img src="./img/upi-box-img.jpeg" alt="fail to load" class="upi-body-img" />
+                <img src="./img/upii.jpg" alt="fail to load" class="upi-body-img" />
 
 
 
