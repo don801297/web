245b3f9a3c7abd8c63c33694eaf979c73a6cbc78
don801297 <don801297@gmail.com>
2024-11-29 10:27:21 +0530

    Update aboutUS.jsx

--- client/src/components/aboutUS.jsx
+++ client/src/components/aboutUS.jsx
@@ -1,40 +1,76 @@
 import React from 'react'
 
 function AboutUS() {
-  return (
-	<div className='contact-body'>
-      
-      
+	return (
+		<div className='contact-body'>
 
-	<div class="contact-container">
-		<div class="text-center">
-			<h1>About Us</h1>
-			<div>
-            We Known as The Gadget Seller - Most Trusted Store of Instagram. We Sell Smartphones & Electronics Gadgets in Affordable Price Range. Since 2020 We're Regularly Delivering Products to Our Customers. Follow Instagram @the The_gadget_seller @Gadgat._.hub to Know More About US.
-			</div>
-		</div>
-		<div class="contact-content">
-			<div class="col-1">
-				
-				
-				<div class="address-line instagram">
-                <i class="fa-brands fa-instagram"></i>
-					<div class="contact-info">
 
-					
-						
-						<a className='insta-link' href="https://instagram.com/the_gadget_sellers"><button className='insta-btn'>Follow </button></a>
+
+			<div class="contact-container">
+				<div class="text-center">
+					<h1>About Us</h1>
+					<div>
+						We Known as The Gadget Seller - Most Trusted Store of Instagram.
+						 We Sell Smartphones & Electronics Gadgets in Affordable Price Range.
+						  Since 2020 We're Regularly Delivering Products to Our Customers.
+						   Follow Me on Instagram @The_gadget_sellers to Know More About US.
 					</div>
 				</div>
+				<div class="contact-content">
+					<div class="col-1">
+
+
+						<div class="address-line instagram">
+							<i class="fa-brands fa-instagram"></i>
+							<div class="contact-info">
+
+
+
+								<a className='insta-link' href="https://instagram.com/the_gadget_sellers"><button className='insta-btn'>Follow </button></a>
+							</div>
+						</div>
+
+						<div class="col-2">
+				<form>
+					<div class="form-container">
+						<h2>Send Message</h2>
+						<div class="form-row">
+							<label>Full Name</label>
+							<div>
+								<input type="text" class="form-field"/>
+							</div>
+						</div>
+						<div class="form-row">
+							<label>Email</label>
+							<div>
+								<input type="text"  class="form-field"/>
+							</div>
+						</div>
+						<div class="form-row">
+							<label>Type your message...</label>
+							<div>
+                            <input type="text" class="form-field"/>
+							</div>
+						</div>
+						<button type="send" class="adress-btn "  value="Send">Submit</button>
+					</div>
+				</form>
 			</div>
-		
+
+					</div>
+
+				
+
+				</div>
+
+				
+
+			</div>
+
+
+
 		</div>
-	</div>
-	
-
-
-    </div>
-  )
+	)
 }
 
 export default AboutUS
